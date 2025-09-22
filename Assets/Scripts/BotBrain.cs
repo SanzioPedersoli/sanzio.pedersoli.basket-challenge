9cd58409e3802b98cd0a0eb3831d004c6ce0a8a7
Sanzio <sanzio.pedersoli@gmail.com>
2025-09-22 17:27:48 +0200

    Bot brain

---
+++ Assets/Scripts/BotBrain.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using System;
+using System.Threading;
+
+[RequireComponent(typeof(Player))]
+public class BotBrain : MonoBehaviour
+{
+    [SerializeField] private BotDifficultyData difficulty;
+    [SerializeField] private MatchManager matchManager;
+
+    private Player player;
+    private CancellationTokenSource cancellationTokenSource;
+
+    private void Awake()
+    {
+        player = GetComponent<Player>();
+        cancellationTokenSource = new CancellationTokenSource();
+        matchManager.gameMode.GameIsOver.AddListener(StopCycle);
+        StartCycle().Forget();
+    }
+
+    private void OnDestroy() => StopCycle();
+
+    private async UniTaskVoid StartCycle()
+    {
+        try
+        {
+            while (!cancellationTokenSource.Token.IsCancellationRequested)
+            {
+                float waitTime = difficulty.GetCurrentWait();
+                await UniTask.Delay(TimeSpan.FromSeconds(waitTime), cancellationToken: cancellationTokenSource.Token);
+                if (cancellationTokenSource.Token.IsCancellationRequested) break;
+                TryToShoot();
+            }
+        }
+        catch (OperationCanceledException){}
+    }
+
+    private void TryToShoot()
+    {
+        if (!player.IsReadyToShoot) return;
+        var error = difficulty.GetError();
+        player.StartShot(error);
+    }
+
+    public void StopCycle()
+    {
+        if (cancellationTokenSource != null && !cancellationTokenSource.IsCancellationRequested)
+        {
+            cancellationTokenSource.Cancel();
+            cancellationTokenSource.Dispose();
+            cancellationTokenSource = null;
+        }
+    }
+}
